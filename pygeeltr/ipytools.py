c5b4d5aac073f962edb52d7431986b7a58f7b055
Rodrigo Principe <fitoprincipe82@gmail.com>
2020-03-09 11:40:30 -0300

    improve

--- pygeeltr/ipytools.py
+++ pygeeltr/ipytools.py
@@ -1,18 +1,18 @@
 # coding=utf-8
 """ Module with some tools for IPython Jupyter Notebook and Lab """
 
-from ipywidgets import HTML, VBox
-from geetools.ui import chart
 import ee
-ee.Initialize()
 
 
-def add2map(landtrendr, map):
+def add2map(landtrendr, map, scale=30):
     """ add a Tab to Map (geetools.ipymap) to plot a LandTrendr result
 
     :param landtrendr: a landtrendr object
     :type landtrendr: landtrendr.LandTrendr
     """
+    from ipywidgets import HTML, VBox
+    import ipygee as ui
+
     # TODO: make it async so it does not block other widgets
     bands = [landtrendr.fit_band+'_fit', landtrendr.fit_band]
     slope = landtrendr.slopes  # .select(bands)
@@ -29,17 +29,16 @@ def add2map(landtrendr, map):
             region = ee.Geometry.Point(coords)
 
             try:
-                ch = chart.Image.series(slope, region,
-                                        bands=bands)
+                ch = ui.chart.Image.series(slope, region, bands=bands,
+                                           scale=scale)
                 ch.title = 'LandTrendr fit\n for point {}'.format(coords)
-                chart_wid = ch.render_widget()
+                chart_wid = ch.renderWidget()
             except Exception as e:
                 chart_wid = HTML('{}'.format(e))
 
             wid.children = [chart_wid]
 
     widget = VBox()
-
     map.addTab('LandTrendr', handler, widget)
 
 
